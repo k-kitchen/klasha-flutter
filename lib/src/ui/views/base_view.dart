b8ef3cd61e84f22036bfafe19dc94d4563cd20dd
emilmammadov <emailmammadov303@gmail.com>
2023-07-11 13:45:33 +0400

    minor reversal

--- lib/src/ui/views/base_view.dart
+++ lib/src/ui/views/base_view.dart
@@ -47,93 +47,91 @@ class _KlashaCheckoutBaseViewState extends State<KlashaCheckoutBaseView> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: SizedBox(
-        height: 500,
-        child: Column(
-          children: [
-            Container(
-              height: 50,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(15.0),
-                  topLeft: Radius.circular(15.0),
-                ),
-                color: appColors.primary,
+    return SizedBox(
+      height: 500,
+      child: Column(
+        children: [
+          Container(
+            height: 50,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(15.0),
+                topLeft: Radius.circular(15.0),
               ),
-              padding: EdgeInsets.symmetric(horizontal: 15.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  if (_currentIndex == 0)
-                    SizedBox.shrink()
-                  else
-                    // back button
-                    KlashaBackButton(
-                      onTap: () {
-                        _bodyPageController.previousPage(
-                          duration: Duration(milliseconds: 500),
-                          curve: Curves.easeInOut,
-                        );
-                      },
-                    ),
-
-                  // close button
-                  KlashaCloseButton(
+              color: appColors.primary,
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                if (_currentIndex == 0)
+                  SizedBox.shrink()
+                else
+                  // back button
+                  KlashaBackButton(
                     onTap: () {
-                      Navigator.pop(context);
-                      _klashaCheckoutResponse = KlashaCheckoutResponse(
-                        message: 'User Cancelled',
-                        status: false,
-                        transactionReference: '',
+                      _bodyPageController.previousPage(
+                        duration: Duration(milliseconds: 500),
+                        curve: Curves.easeInOut,
                       );
                     },
                   ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: AnimatedSwitcher(
-                duration: Duration(milliseconds: 500),
-                transitionBuilder: (Widget child, Animation<double> animation) {
-                  return ScaleTransition(scale: animation, child: child);
-                },
-                child: _klashaCheckoutResponse == null
-                    ? CheckoutViewWrapper(
-                        email: widget.email,
-                        amount: widget.amount,
-                        checkoutCurrency: widget.checkoutCurrency,
-                        onCheckoutResponse: (KlashaCheckoutResponse response) {
-                          _klashaCheckoutResponse = response;
-                          widget.onComplete(response);
-                          setState(() {});
-                        },
-                        bodyPageController: _bodyPageController,
-                        onPageChanged: (newIndex) {
-                          setState(() {});
 
-                          _currentIndex = newIndex;
-                        },
-                        environment: widget.environment,
-                      )
-                    : PaymentStatusView(
-                        paymentStatus: _klashaCheckoutResponse!.status,
-                        onAction: () {
-                          if (_klashaCheckoutResponse!.status) {
-                            Navigator.pop(context);
-                          } else {
-                            _klashaCheckoutResponse = null;
-                          }
-                          setState(() {});
-                        },
-                      ),
-              ),
+                // close button
+                KlashaCloseButton(
+                  onTap: () {
+                    Navigator.pop(context);
+                    _klashaCheckoutResponse = KlashaCheckoutResponse(
+                      message: 'User Cancelled',
+                      status: false,
+                      transactionReference: '',
+                    );
+                  },
+                ),
+              ],
             ),
-            SecuredByKlasha(),
-            const SizedBox(height: 10),
-          ],
-        ),
+          ),
+          Expanded(
+            child: AnimatedSwitcher(
+              duration: Duration(milliseconds: 500),
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return ScaleTransition(scale: animation, child: child);
+              },
+              child: _klashaCheckoutResponse == null
+                  ? CheckoutViewWrapper(
+                      email: widget.email,
+                      amount: widget.amount,
+                      checkoutCurrency: widget.checkoutCurrency,
+                      onCheckoutResponse: (KlashaCheckoutResponse response) {
+                        _klashaCheckoutResponse = response;
+                        widget.onComplete(response);
+                        setState(() {});
+                      },
+                      bodyPageController: _bodyPageController,
+                      onPageChanged: (newIndex) {
+                        setState(() {});
+
+                        _currentIndex = newIndex;
+                      },
+                      environment: widget.environment,
+                    )
+                  : PaymentStatusView(
+                      paymentStatus: _klashaCheckoutResponse!.status,
+                      onAction: () {
+                        if (_klashaCheckoutResponse!.status) {
+                          Navigator.pop(context);
+                        } else {
+                          _klashaCheckoutResponse = null;
+                        }
+                        setState(() {});
+                      },
+                    ),
+            ),
+          ),
+          SecuredByKlasha(),
+          const SizedBox(height: 10),
+        ],
       ),
     );
   }
