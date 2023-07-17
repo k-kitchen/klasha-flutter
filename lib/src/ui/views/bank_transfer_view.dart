4ecac0433667dbef75f5c2a3b1bad0405c73fbc4
emilmammadov <emailmammadov303@gmail.com>
2023-07-17 21:45:03 +0400

    bank_transfer_view user_details

--- lib/src/ui/views/bank_transfer_view.dart
+++ lib/src/ui/views/bank_transfer_view.dart
@@ -3,7 +3,10 @@ import 'package:klasha_checkout/src/core/core.dart';
 import 'package:klasha_checkout/src/core/services/bank_transfer/bank_transfer_service_impl.dart';
 import 'package:klasha_checkout/src/shared/shared.dart';
 import 'package:klasha_checkout/src/ui/widgets/bank_account_details_widget.dart';
+import 'package:klasha_checkout/src/ui/widgets/buttons/buttons.dart';
 import 'package:klasha_checkout/src/ui/widgets/retry_widget.dart';
+
+import '../widgets/widgets.dart';
 
 class BankTransferCheckoutView extends StatefulWidget {
   const BankTransferCheckoutView({super.key, required this.config});
@@ -16,21 +19,36 @@ class BankTransferCheckoutView extends StatefulWidget {
 }
 
 class _BankTransferCheckoutViewState extends State<BankTransferCheckoutView> {
-  late Future<ApiResponse<BankAccountDetails>> futureBankDetails;
+  Future<ApiResponse<BankAccountDetails>>? futureBankDetails;
+  late PageController pageController;
+  int currentPage = 0;
+  var formKey = GlobalKey<FormState>();
+  String? fullName, email, phoneNumber;
 
   @override
   void initState() {
     super.initState();
-    _fetchBankDetails();
+    email = widget.config.email;
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  void _onPageChanged(int newPage) {
+    setState(() => currentPage = newPage);
   }
 
   void _fetchBankDetails() async {
     BankTransferBody bankTransferBody = BankTransferBody(
       txRef:
           'klasha-fund-wallet-from-bank-transfer-${DateTime.now().microsecondsSinceEpoch}',
-      email: widget.config.email,
+      email: email,
       amount: widget.config.amount,
-      phoneNumber: widget.config.phone,
+      phoneNumber: phoneNumber,
       narration: 'Bank Transfer to fund wallet',
       currency: widget.config.checkoutCurrency.name,
     );
@@ -66,43 +84,76 @@ class _BankTransferCheckoutViewState extends State<BankTransferCheckoutView> {
           ),
           const SizedBox(height: 20),
           Expanded(
-            child: FutureBuilder<ApiResponse<BankAccountDetails>>(
-              future: futureBankDetails,
-              builder: (BuildContext context,
-                  AsyncSnapshot<ApiResponse<BankAccountDetails>> snapshot) {
-                Widget widgetToShow;
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                    widgetToShow = Center(
-                      child: Container(
-                        width: 25.0,
-                        height: 25.0,
-                        margin: const EdgeInsets.symmetric(vertical: 30.0),
-                        child: CircularProgressIndicator(strokeWidth: 3.0),
-                      ),
-                    );
-                    break;
-                  case ConnectionState.done:
-                    if (snapshot.hasData && snapshot.data?.data != null) {
-                      var data = snapshot.data!.data!;
-                      widgetToShow = BankAccountDetailsWidget(
-                        bankAccountNumber: data.transferAccount!,
-                        bankName: data.transferBank!,
-                      );
-                    } else {
-                      widgetToShow = Center(
-                        child: RetryWidget(onRetry: _fetchBankDetails),
-                      );
+            child: PageView(
+              controller: pageController,
+              onPageChanged: _onPageChanged,
+              clipBehavior: Clip.none,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                Column(
+                  children: [
+                    UserContactForm(
+                      onFullNameChanged: (val) => fullName = val,
+                      onEmailChanged: (val) => email = val,
+                      onPhoneNumberChanged: (val) => phoneNumber = val,
+                      initialEmail: widget.config.email,
+                      formKey: formKey,
+                    ),
+                    const SizedBox(height: 20),
+                    KlashaPrimaryButton(
+                      text: 'Continue',
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          _fetchBankDetails();
+                          pageController.nextPage(
+                            duration: Duration(milliseconds: 300),
+                            curve: Curves.easeIn,
+                          );
+                          FocusScope.of(context).unfocus();
+                        }
+                      },
+                    ),
+                  ],
+                ),
+                FutureBuilder<ApiResponse<BankAccountDetails>>(
+                  future: futureBankDetails,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<ApiResponse<BankAccountDetails>> snapshot) {
+                    Widget widgetToShow;
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.waiting:
+                        widgetToShow = Center(
+                          child: Container(
+                            width: 25.0,
+                            height: 25.0,
+                            margin: const EdgeInsets.symmetric(vertical: 30.0),
+                            child: CircularProgressIndicator(strokeWidth: 3.0),
+                          ),
+                        );
+                        break;
+                      case ConnectionState.done:
+                        if (snapshot.hasData && snapshot.data?.data != null) {
+                          var data = snapshot.data!.data!;
+                          widgetToShow = BankAccountDetailsWidget(
+                            bankAccountNumber: data.transferAccount!,
+                            bankName: data.transferBank!,
+                          );
+                        } else {
+                          widgetToShow = Center(
+                            child: RetryWidget(onRetry: _fetchBankDetails),
+                          );
+                        }
+                        break;
+                      default:
+                        widgetToShow = Center(
+                          child: RetryWidget(onRetry: _fetchBankDetails),
+                        );
+                        break;
                     }
-                    break;
-                  default:
-                    widgetToShow = Center(
-                      child: RetryWidget(onRetry: _fetchBankDetails),
-                    );
-                    break;
-                }
-                return widgetToShow;
-              },
+                    return widgetToShow;
+                  },
+                ),
+              ],
             ),
           ),
         ],
